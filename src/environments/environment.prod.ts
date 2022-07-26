fbc635c33fee5417588058e52bf1aba42795a12c
Osler2021 <gmonti@gmail.com>
2022-07-25 23:29:52 -0300

    Version final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost:8080",
-  apiAuthUrl: "http://localhost:8080/auth/"
+  //apiBaseUrl: "http://localhost:8080",
+  //apiAuthUrl: "http://localhost:8080/auth/"
+  apiBaseUrl: "https://portfoliogmonti.herokuapp.com",
+  apiAuthUrl: "https://portfoliogmonti.herokuapp.com/auth/"
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,10 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: "http://localhost:8080",
-  apiAuthUrl: "http://localhost:8080/auth/"
+  //apiBaseUrl: "http://localhost:8080",
+  //apiAuthUrl: "http://localhost:8080/auth/"
+  apiBaseUrl: "https://portfoliogmonti.herokuapp.com",
+  apiAuthUrl: "https://portfoliogmonti.herokuapp.com/auth/"
 };
 
 
